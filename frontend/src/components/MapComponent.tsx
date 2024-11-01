e344a6e55085c2b5e4aede7aa861573f2502a502
SorrowOfSpira <francomatamala1@hotmail.com>
2024-11-01 10:02:35 -0300

    mapa actualizado

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -15,22 +15,22 @@ const MapComponent: React.FC = () => {
 
     useEffect(() => {
         // Función para obtener los nodos desde la API
-        const fetchNodos = async () => {
-            try {
-                const response = await axios.get('http://127.0.0.1:8000/docs#/Nodos/');
-                setNodos(response.data);
-            } catch (error) {
-                console.error("Error al obtener los nodos:", error);
-            }
-        };
+        // const fetchNodos = async () => {
+        //     try {
+        //         // const response = await axios.get('http://127.0.0.1:8000/nodos/');
+        //         setNodos(response.data);
+        //     } catch (error) {
+        //         console.error("Error al obtener los nodos:", error);
+        //     }
+        // };
 
-        fetchNodos();
+        // fetchNodos();
 
         // Inicializar el mapa
-        const map = L.map('map').setView([-43.2530, -65.3016], 12); // Centrro
+        const map = L.map('map').setView([-43.420000, -65.790000], 10); // Centro
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
+            maxZoom: 18,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
@@ -42,26 +42,37 @@ const MapComponent: React.FC = () => {
             [-43.33826, -65.21180], // Vértice 4
         ];
 
+        
         // Crear el polígono y agregarlo al mapa
-        const polygon = L.polygon(polygonCoords, { color: 'purple', fillOpacity: 0.3 }).addTo(map);
-        polygon.bindPopup('Área delimitada: Zona de interés');
+        // const polygon = L.polygon(polygonCoords, { color: 'purple', fillOpacity: 0.3 }).addTo(map);
+        // polygon.bindPopup('Área delimitada: Zona de interés');
 
         // Establecer límites máximos del mapa
         const bounds = L.latLngBounds(polygonCoords);
         map.setMaxBounds(bounds);
 
-         // Agregar puntos de interés desde la API
-        /* nodos.forEach(nodo => {
-            const { latitud, longitud } = nodo; // Desestructurar la posición
-             L.marker([latitud, longitud])
-                .addTo(map)
-                .bindPopup(`Nodo `); // Ajustar el nombre según tus necesidades 
-        }); */
+        //  // Agregar puntos de interés desde la API
+        //  for (let i = 0; i < nodos.length; i+1) {
+        //     const { latitud, longitud } = nodos[i]; // Desestructurar la posición
+        //     L.marker([latitud, longitud])
+        //         .addTo(map)
+        //         .bindPopup(`Nodo`); // Ajustar el nombre según tus necesidades 
+        // }
+        
 
         // Marcado manual
-        L.marker([-43.33826, -65.21180])
+         L.marker([-43.410000, -65.920000])
                 .addTo(map)
                 .bindPopup(`Nodo 1`); // Ajustar el nombre según tus necesidades
+        L.marker([-43.350000, -65.502000])
+                .addTo(map)
+                .bindPopup(`Nodo 2`); // Ajustar el nombre según tus necesidades
+        L.marker([-43.380000, -65.650000])
+                .addTo(map)
+                .bindPopup(`Nodo 3`); // Ajustar el nombre según tus necesidades
+        L.marker([-43.420000, -65.790000])
+                .addTo(map)
+                .bindPopup(`Nodo 4`); // Ajustar el nombre según tus necesidades
 
         return () => {
             map.remove();
